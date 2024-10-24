005fde78f3826dae8cf74711921e0ce4224f3592
= <=>
2024-10-24 11:26:34 +0200

    Creando la función que dara forma a la cuadricula, es decir, a la estructura inicial del tablero del juego de Buscaminas(En proceso).

--- code/src/components/buscaMinas/buscaMinas.jsx
+++ code/src/components/buscaMinas/buscaMinas.jsx
@@ -1,6 +1,22 @@
 import HomeMenuButton from "../common/HomeMenuButton";
 
 function BuscaMinas({screenChanger}) {
+  // Función para crear la cuadrícula
+    const createGrid = () => {
+        let newGrid = [];
+        for (let row = 0; row < rows; row++) {
+        let newRow = [];
+        for (let col = 0; col < cols; col++) {
+            newRow.push({
+            revealed: false,
+            mine: false,
+            adjacentMines: 0
+            });
+        }
+        newGrid.push(newRow);
+        }
+        return newGrid;
+    };
     return (
         <div>
             <h1>BuscaMinas</h1>
