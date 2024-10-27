1cdee851743f95508211c5e5e780cdcdfdb66465
Marta21GH <martaprietorodriguezdevera@gmail.com>
2024-10-27 17:26:37 +0100

    cambios en el jsx

--- code/src/components/spaceInvaders/SpaceInvaders.jsx
+++ code/src/components/spaceInvaders/SpaceInvaders.jsx
@@ -1,15 +1,34 @@
+import { useEffect, useRef } from 'react';
 import HomeMenuButton from "../common/HomeMenuButton";
+import { gameLoop } from './game-loop'; // Importamos solo gameLoop
+import * as config from './config'; // Importamos todos los exports de config como un objeto
 
-function SpaceInvaders({screenChanger}) {
+function SpaceInvaders({ screenChanger }) {
+    const canvasRef = useRef(null);
+
+    useEffect(() => {
+        const canvas = canvasRef.current;
+        const context = canvas.getContext('2d');
+
+        // Inicialización del juego y bucle principal
+        const loop = () => {
+            gameLoop(context, config);
+            requestAnimationFrame(loop);
+        };
+        loop();
+
+        return () => {
+            // Cleanup si es necesario
+        };
+    }, []);
 
     return (
         <div>
-            <h1>Space invaders</h1>
-            <p>WIP</p>
-            <HomeMenuButton screenChanger={screenChanger}/>
+            <h1>Space Invaders</h1>
+            <canvas ref={canvasRef} width={config.worldWidth} height={config.worldHeight} />
+            <HomeMenuButton screenChanger={screenChanger} />
         </div>
     );
-
 }
 
 export default SpaceInvaders;
