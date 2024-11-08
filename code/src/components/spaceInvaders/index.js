cf9472a1ea94c14803f00a9ca90d7ad94dae38a8
Eduardo Rueda de Santiago <eduardoruedadesantiago@gmail.com>
2024-11-08 09:13:47 +0100

    Fixed space invaders files

--- code/src/components/spaceInvaders/index.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import SpaceInvaders from './space-invaders';
-
-const App = () => <SpaceInvaders />
-const rootElement = document.getElementById("root");
-
-ReactDOM.render(<App />, rootElement);
